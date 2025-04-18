88d76e54121ba63a468b26cacb571173fabff312
Lennart <lennartvandermolen@gmail.com>
2025-04-18 20:45:09 +0000

    chore: purge demo code and scaffold core components

---
+++ components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <div className="h-full w-full flex items-center justify-center
+                    bg-gray-800 text-white text-6xl font-bold">
+      Hero
+    </div>
+  );
+}
---
+++ components/MedicalCarousel.jsx
@@ -0,0 +1,4 @@
+// (Paste your finalized MedicalCarousel code here)
+export default function MedicalCarousel({ reverse = false }) {
+  return <div>…your carousel here…</div>;
+}
