b9aad0a2cfd8239af7710246576bc5959b3ec758
Lennart <lennartvandermolen@gmail.com>
2025-04-25 17:08:34 +0000

    feat(neoflix): sticky sidebar + tall 12-section demo

--- components/ui/Section.jsx
+++ components/ui/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ id, children }) {
   return (
-    <section id={id} className="scroll-mt-24 py-32 first:pt-0 last:pb-0">
+    <section className="min-h-screen flex flex-col justify-center" id={id} className="scroll-mt-24 py-32 first:pt-0 last:pb-0">
       {children}
     </section>
   );
