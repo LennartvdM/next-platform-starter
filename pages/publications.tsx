8e629042e9ab97c494de53c4b3fe6576f46289fc
Lennart <lennartvandermolen@gmail.com>
2025-04-25 14:58:13 +0000

    chore: remove legacy pages router (fix build)

--- pages/publications.tsx
+++
@@ -1,8 +0,0 @@
-export default function Publications() {
-  return (
-    <div className="p-10">
-      <h1 className="text-4xl font-bold">Publications</h1>
-      <p className="mt-4 text-slate-600">Research list coming soon.</p>
-    </div>
-  );
-}
--- pages/toolbox.tsx
+++
@@ -1,9 +0,0 @@
-export default function Toolbox() {
-  return (
-    <iframe
-      src="https://your-gitbook-url"
-      title="Toolbox"
-      style={{ width: '100%', height: '100vh', border: 'none' }}
-    />
-  );
-}
